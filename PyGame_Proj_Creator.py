ddd5460a979d1be173fb7a4b0245564869b13ed7
=jorgengullstrand <=>
2022-04-01 13:40:34 +0200

    added comments

--- PyGame_Proj_Creator.py
+++ PyGame_Proj_Creator.py
@@ -16,21 +16,20 @@ if not os.path.exists(filename):
 
         # Writes everything to main.py
         with open(main_file, 'w') as file:
-            file.write("import pygame\n")                                       # yup imports pygame
-            file.write("background_color = (255,255,255)\n")                    # Defines the background color
-            file.write("(width, height) = (1280, 720)\n")                       # defines the resolution
-            file.write("screen = pygame.display.set_mode((width, height))\n")   # Sets the resolution
-            file.write("pygame.display.set_caption('" + filename + "')\n")      # Sets window name
-            file.write("screen.fill(background_color)\n")                       # Sets the background color 
-            file.write("pygame.display.flip()\n")           
-            file.write("running = True\n")  
-            file.write("while running:\n")                                      # Main game loop
-            file.write("    for event in pygame.event.get():\n")
-            file.write("        if event.type == pygame.QUIT:\n")               # Checks if quit event triggered
-            file.write("            running = False\n")                         # Nope running shouldn't be True    
+            file.write("import pygame                                           # yup imports pygame\n")
+            file.write("background_color = (255,255,255)                        # Defines the background color\n")
+            file.write("(width, height) = (1280, 720)                           # defines the resolution\n")
+            file.write("screen = pygame.display.set_mode((width, height))       # Sets the resolution\n")
+            file.write("pygame.display.set_caption('" + filename + "')          # Sets window name\n")
+            file.write("screen.fill(background_color)                           # Sets the background color\n")
+            file.write("pygame.display.flip()                                   \n")           
+            file.write("running = True                                          \n")  
+            file.write("while running:                                          # Main game loop\n")
+            file.write("    for event in pygame.event.get():                    \n")
+            file.write("        if event.type == pygame.QUIT:                   # Checks if quit event triggered\n")
+            file.write("            running = False                             # Nope running shouldn't be True\n")    
             file.close()
             
         # Writes the batch file to run the game
         with open(batch_file, "w") as file:
-            file.write("python code/main.py")                                   # sets the path to main.py for the batch
-            
+            file.write("python code/main.py                                     # sets the path to main.py for the batch")
