4407ad26ef7b3dfcfc747338f65dea48a5740395
=jorgengullstrand <=>
2022-04-01 14:10:45 +0200

    small changes

--- PyGame_Proj_Creator.py
+++ PyGame_Proj_Creator.py
@@ -16,13 +16,13 @@ if not os.path.exists(filename):
 
         # Writes everything to main.py
         with open(main_file, 'w') as file:
-            file.write("import pygame                                           # yup imports pygame\n")
+            file.write("import pygame                                           # yup imports pygame\n\n")
             file.write("background_color = (255,255,255)                        # Defines the background color\n")
             file.write("(width, height) = (1280, 720)                           # defines the resolution\n")
-            file.write("screen = pygame.display.set_mode((width, height))       # Sets the resolution\n")
+            file.write("screen = pygame.display.set_mode((width, height))       # Sets the resolution\n\n\n")
             file.write("pygame.display.set_caption('" + filename + "')          # Sets window name\n")
             file.write("screen.fill(background_color)                           # Sets the background color\n")
-            file.write("pygame.display.flip()                                   \n")           
+            file.write("pygame.display.flip()                                   \n\n\n")           
             file.write("running = True                                          \n")  
             file.write("while running:                                          # Main game loop\n")
             file.write("    for event in pygame.event.get():                    \n")
@@ -32,4 +32,4 @@ if not os.path.exists(filename):
             
         # Writes the batch file to run the game
         with open(batch_file, "w") as file:
-            file.write("python code/main.py                                     # sets the path to main.py for the batch")
+            file.write("python code/main.py")                                   # sets the path to main.py for the batch
